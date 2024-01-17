efa14aae941983f8d94a3375abf8dbdb68ed5a7d
Reydson Martins <reydson.eduardo@gmail.com>
2024-01-17 00:01:54 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Components
+
+- []Text
+- []Heading
+- []Box
+- []Button
+- []Textlnput
+- []TextArea
+- []Checkbox
+- []Avatar
+- []MultiStep
+
+-- instalando pacote tsup - npm i tsup -D | ele converte o cod para versoes(mjs, e comon js) do js, e tudo dentro de uma pasta.
---
+++ packges/tokens/src/index.ts
@@ -0,0 +1,12 @@
+export * from './colors'
+
+export * from './space'
+export * from './radii'
+
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
+
+// import {colors} from '@ignite-ui/tokens/colors'
+// import {colors} from '@ignite-ui/tokens' - com o index
